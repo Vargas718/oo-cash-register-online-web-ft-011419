e36b0795587327332faee2bcd2afdb1aedc10dbb
Rafael Vargas <vargas718@gmail.com>
2019-03-20 21:24:32 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -17,18 +17,29 @@ end
   end
   
   def void_last_transaction
+<<<<<<< HEAD
   @total -= @total
   
 # binding.pry
   end
 
+=======
+  @total.pop
+  
+
+end
+>>>>>>> 1946d5e63cb8c29717af5a61975dc218be6d993b
 
 def apply_discount
   if discount == 0 
     "There is no discount to apply."
   else
 "After the discount, the total comes to $#{@total -= (@total * discount.to_f/100).round}."
+<<<<<<< HEAD
   end
+=======
+
+>>>>>>> 1946d5e63cb8c29717af5a61975dc218be6d993b
   end
 end
 
